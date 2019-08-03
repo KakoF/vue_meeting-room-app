a522ed5318a1f667c96dac1e0b3c05eaeefb24ff
Marcos <kakoferrare87@gmail.com>
2019-08-03 19:10:20 -0300

    Aplicação quase definida para persistir usuário

---
+++ src/modules/dashboard/router/index.js
@@ -0,0 +1,10 @@
+const Dashboard = () => import('./../views/Dashboard.vue')
+export default [
+    {
+        path: '/dashboard',
+        component: Dashboard,
+        meta: {
+            requiresAuth: true
+        }
+    }
+]
--- src/router.js
+++ src/router.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import authRoutes from '@/modules/auth/router'
+import dashboardRoutes from '@/modules/dashboard/router'
+import store from '@/store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     ...authRoutes,
-    
+    ...dashboardRoutes,
     { path: '', redirect: '/login' }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
